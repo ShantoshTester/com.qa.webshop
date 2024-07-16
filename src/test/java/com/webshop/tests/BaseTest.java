0800f1f3c5ea73e86ce7e1d8df2b70d05267c762
shantosh <shantosh25@hotmail.com>
2024-07-16 06:53:58 +0530

    added dataprovider in the base test

--- src/test/java/com/webshop/tests/BaseTest.java
+++ src/test/java/com/webshop/tests/BaseTest.java
@@ -7,6 +7,8 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
@@ -14,6 +16,7 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.webshop.base.DriverScript;
 import com.webshop.pages.HomePage;
 import com.webshop.pages.LoginPage;
+import com.webshop.utils.ExcelUtil;
 import com.webshop.utils.HelperUtil;
 
 public class BaseTest extends DriverScript {
@@ -55,6 +58,23 @@ public class BaseTest extends DriverScript {
 		}
 		report.flush();
 		quitDriver();
+	}
+	
+	@DataProvider(name="wsdata")
+	public Object[][] testData()
+	{
+		ExcelUtil excel = new ExcelUtil("./src/test/resources/testdata/wsdata.xlsx");
+		int rows = excel.getRowCount(0);
+		
+		Object data[][] = new Object[rows][2];
+		
+		for(int i=0; i<rows; i++)
+		{
+			data[i][0] = excel.getCellData(0, i, 0);
+			data[i][1] = excel.getCellData(0, i, 1);
+		}
+		
+		return data;
 	}
 
 }
